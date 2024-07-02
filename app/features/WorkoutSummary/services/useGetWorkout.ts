8a01f7b40736ad06ac8206e8e4cf5b2645aaa016
Alex Cheng <27754503+ktalexcheng@users.noreply.github.com>
2024-07-03 02:19:33 +0800

    disable refetch on WorkoutSummaryScreen mount for better UX

--- app/features/WorkoutSummary/services/useGetWorkout.ts
+++ app/features/WorkoutSummary/services/useGetWorkout.ts
@@ -1,6 +1,4 @@
-import { useFocusEffect } from "@react-navigation/native"
 import { useQuery } from "@tanstack/react-query"
-import React from "react"
 import { queries } from "./queryFactory"
 
 export const useGetWorkout = (workoutId?: string) => {
@@ -9,11 +7,11 @@ export const useGetWorkout = (workoutId?: string) => {
   // In case the workout is updated, we want to refresh every time the screen is focused
   // For a more generic refetch on focus hook that can be shared for other queries, see:
   // https://gist.github.com/Luccasoli/9b4ec6fbe87a702d9e87a09fa0b38d59
-  useFocusEffect(
-    React.useCallback(() => {
-      query.refetch()
-    }, []),
-  )
+  // useFocusEffect(
+  //   React.useCallback(() => {
+  //     query.refetch()
+  //   }, []),
+  // )
 
   return query
 }
